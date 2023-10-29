18d70c03a90ea04c29f826bd3b468f608feb6834
GrayCoded <130934177+GrayCoded@users.noreply.github.com>
2023-10-29 12:20:44 -0300

    made additions to the components and looking at adding tailwind to the vite framework

---
+++ Portfolio/src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Footer = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    if (location.pathname !== '/') {
+      navigate(-1);
+    }
+  };
+
+  return (
+    <footer
+      className="avoid-particle bg-black text-white  text-center"
+      style={{ position: 'fixed', bottom: 0, width: '100%' }}
+    >
+      <div className="  mb-1">
+        {location.pathname !== '/' && (
+          <button className="btn btn-dark mb-3" onClick={handleGoBack}>
+            &larr; Go Back
+          </button>
+        )}
+        <h4>Dan Grayson</h4>
+      </div>
+      {/* Additional footer content can be added here */}
+      <p>&copy; {new Date().getFullYear()} </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ Portfolio/src/components/Header.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from 'react';
+const Header = () => {
+    const [nav, setNav] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+    useEffect(() => {
+      setIsLoggedIn(Auth.loggedIn());
+    }, []);
+    const logout = () => {
+      if (isLoggedIn) {
+        Auth.logout();
+      }
+      return;
+    };
+  
+    const navs = [
+      {
+        id: 1,
+        icon: <FaHome className="mx-2" />,
+        text: 'DASHBOARD',
+        link: '/dashboard',
+      },
+      {
+        id: 2,
+        icon: <FaUser className="mx-2" />,
+        text: 'PROFILE',
+        link: '/user/me',
+      },
+    ];
+  
+    return (
+      <div className="flex justify-between items-center w-full p-3 mb-10 bg-gradient-to-b from-opacityBlack sticky top-0">
+        <div className="flex justify-center items-center gap-3">
+          <a href="/" className="avoid-particle">
+            <img src={cyberpic} alt="Cyber Script" width={400} />
+          </a>
+        </div>
+        <div className="z-50">
+          <ul className="avoid-particle gap-x-5 text-2xl text-white hidden md:flex mr-5 p-4 bg-opacityBlack border-2 border-cyber-yellow rounded-tl-3xl rounded-br-3xl">
+            {navs.map(({ id, icon, text, link }) => (
+              <a href={link} key={id}>
+                <li className="flex items-center justify-center cursor-pointer hover:text-cyber-pink duration-300">
+                  {icon} {text}
+                </li>
+              </a>
+            ))}
+            <a href={isLoggedIn ? '/' : '/login'} onClick={logout}>
+              <li className="flex items-center justify-center cursor-pointer hover:text-cyber-pink duration-300">
+                {isLoggedIn ? (
+                  <FaSignInAlt className="mx-2" />
+                ) : (
+                  <FaUserShield className="mx-2" />
+                )}
+                {isLoggedIn ? 'LOG_OUT' : 'LOG_IN'}
+              </li>
+            </a>
+          </ul>
+          <div
+            onClick={() => setNav(!nav)}
+            className="text-white cursor-pointer pr-3 md:hidden"
+          >
+            {!nav ? <FaBars size={35} /> : <FaTimes size={35} />}
+          </div>
+          {nav && (
+            <div className="z-50">
+              <ul className="text-6xl sm:hidden gap-y-12 flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-opacityBlack to-opacityLightBlack">
+                {navs.map(({ id, icon, text, link }) => (
+                  <a href={link} key={id}>
+                    <li className="text-white text-4xl flex items-center justify-center cursor-pointer hover:scale-125 duration-500">
+                      {icon} {text}
+                    </li>
+                  </a>
+                ))}
+                <FaTimes
+                  size={35}
+                  onClick={() => setNav(!nav)}
+                  className="text-white absolute top-9 right-6 cursor-pointer"
+                />
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  };
+  
+  export default Header;
+  
